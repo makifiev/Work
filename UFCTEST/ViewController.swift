9b143922b1635b1dc9dbb7746ec8147a3c05fe36
Максим Акифьев <m.akifiev@yandex.ru>
2020-07-30 15:54:50 +0300

    Initial Commit

---
+++ UFCTEST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UFCTEST
+//
+//  Created by Акифьев Максим  on 30/07/2020.
+//  Copyright © 2020 Акифьев Максим . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
